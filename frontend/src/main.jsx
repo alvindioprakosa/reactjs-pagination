7b529b1e5b77b7bf3f3a6f13caf6278866ba93ef
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-30 08:59:34 +0700

    Update Main JSX

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,16 @@ import App from "./App.jsx";
 import "bulma/css/bulma.css";
 import axios from "axios";
 
-axios.defaults.headers.post["Content-Type"] =
-  "application/x-www-form-urlencoded";
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-axios.defaults.timeout = import.meta.env.VITE_API_TIMEOUT;
+// Buat instance axios dengan konfigurasi default
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000", // Tambahkan fallback
+  timeout: import.meta.env.VITE_API_TIMEOUT
+    ? parseInt(import.meta.env.VITE_API_TIMEOUT, 10)
+    : 5000, // Fallback timeout ke 5000ms jika tidak ada
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
